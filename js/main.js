3885ec8b5b2cddd1e0b65c6e50ea87cbd6759c37
Liam Eller <liamthelastson@gmail.com>
2022-01-28 09:39:21 -0800

    Pass Pizza.prototype.checkForSize test

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
 }
 let pizza = new Pizza("20'", ["red-sauce", "cheese"]);
-console.log(pizza.size)
+// console.log(pizza.size)
+
+Pizza.prototype.checkForSize = function() {
+  let total = 0;
+  if (this.size === "18'") {
+    total += 20;
+  } else if (this.size === "24'") {
+    total += 25;
+  } else {
+    total += 30;
+  }
+  console.log(total)
+}
+
+pizza.checkForSize();
