7a95a3d31112e25974c2319320ae73950626bf25
Liam Eller <liamthelastson@gmail.com>
2022-01-28 14:43:52 -0800

    Add jquery to grab user input and styling including a gif

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-class Pizza{
-  constructor(size, toppings) {
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.price = 0;
 }
-
-  checkForSize() {
+Pizza.prototype.checkForSize = function() {
     if (this.size === "18'") {
       this.price += 20;
     } else if (this.size === "24'") {
@@ -14,23 +12,28 @@ class Pizza{
       this.price += 30;
     }
     this.checkForToppings();
-  }
-  
-  checkForToppings() { 
-    let that = this;
-    this.toppings.forEach(function(i) { 
-      if (i === "prosciutto" || i === "sausage" || i === "pepperoni") {  
-        that.price += 3;  
-      } else if (i === "artichokes" || i === "mama lil peppers") {  
-        that.price += 2;  
-      } else {  
-        that.price += 1; 
-      }  
-    });  
   } 
-}
+Pizza.prototype.checkForToppings = function() { 
+  let that = this;
+  this.toppings.forEach(function(i) { 
+    if (i === "prosciutto" || i === "sausage" || i === "pepperoni") {  
+      that.price += 3;  
+    } else if (i === "artichokes" || i === "mama lil peppers") {  
+      that.price += 2;  
+    } else {  
+      that.price += 1; 
+    }  
+  });  
+} 
 
-let pizza = new Pizza("24'", ["red-sauce", "artichokes", "pepperoni"]);
-console.log(pizza.price)
-pizza.checkForSize();
-console.log(pizza.price)
+$(document).ready(function() {
+  $("form#pizza-order").submit(function(event) {
+    event.preventDefault();
+    let pizzaSize = $("input:radio[name=size]:checked").val();
+    let pizzaToppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      pizzaToppings.push($(this).val());
+    });
+    let pizzaOrder = new Pizza(pizzaSize, pizzaToppings);
+  });
+});
